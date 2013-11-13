c294d77d84778df8a7fdc847b6b07aec2ad3d0cb
Mayorov Andrey <avmayorov@platbox.com>
2013-11-13 20:48:00 +0400

    Alarm: simple mailer alarmist, abuses gen_smtp_client

---
+++ src/deathtoll_alarm_mailer.erl
@@ -0,0 +1,66 @@
+%%
+%% Mail alarmist
+
+%% Relies on the `gen_smtp` SMTP library, please do not forget to depend
+%% on it if you wish to use this alarmist.
+%%
+%% https://github.com/Vagabond/gen_smtp.git
+
+-module(deathtoll_alarm_mailer).
+-behaviour(deathtoll_alarmist).
+
+-export([
+    init/2,
+    alarm/3,
+    terminate/2
+]).
+
+-record(state, {
+    to :: [email_address()],
+    from :: {email_address(), string()},
+    opts :: [any()],
+    alarm :: undefined | deathtoll:alarm()
+}).
+
+-type state() :: #state{}.
+-type email_address() :: string().
+
+-spec init(deathtoll:cref(), deathtoll:options()) -> {ok, state()}.
+
+init(_Ref, Options) ->
+    {[From, To, Me], SmtpOptions} = deepprops:split([from, to, {me, "Deathtoll"}], Options),
+    _ = is_email(From) orelse error({badarg, From}),
+    _ = lists:all(fun is_email/1, To) orelse error({badarg, To}),
+    State = #state{
+        to = To,
+        from = {From, Me},
+        opts = SmtpOptions
+    },
+    {ok, State}.
+
+-spec alarm(deathtoll:cref(), deathtoll:alarm(), state()) -> {ok, state()}.
+
+alarm(Ref, Alarm, State = #state{from = {From, Me}, to = To, opts = Options, alarm = WasAlarm}) ->
+    Text = deathtoll_formatter:format_alarm(Ref, Alarm, WasAlarm),
+    Body = "Subject: " ++ deathtoll_formatter:format_ref(Ref) ++ "\r\n"
+        "From: " ++ Me ++ "\r\n\r\n" ++ Text,
+    EMail = {From, To, Body},
+    _Pid = gen_smtp_client:send(EMail, Options, fun report/1),
+    {ok, State#state{alarm = Alarm}}.
+
+report({ok, Receipt}) ->
+    error_logger:info_msg("Mail sent: ~p", [Receipt]);
+
+report(Error) ->
+    error_logger:error_msg("Mail sending failed: ~p", [Error]).
+
+-spec terminate(deathtoll:cref(), state()) -> ok.
+
+terminate(_Ref, _State) ->
+    ok.
+
+%%
+
+is_email(String) ->
+    %% just checks that it looks like an email, nothing special
+    nomatch =/= re:run(String, <<"^[\\w+-_.]+@[a-z-_.]+[^.]$">>, [{capture, none}]).
---
+++ src/deathtoll_formatter.erl
@@ -0,0 +1,53 @@
+%%
+%% Default alert-to-text formatter
+
+-module(deathtoll_formatter).
+
+-export([
+    format_alarm/2,
+    format_alarm/3,
+    format_ref/1
+]).
+
+-spec format_alarm(deathtoll:cref(), deathtoll:alarm()) ->
+    string().
+
+format_alarm(Ref, Alarm) ->
+    format_alarm(Ref, Alarm, undefined).
+
+-spec format_alarm(deathtoll:cref(), deathtoll:alarm(), WasAlarm :: undefined | deathtoll:alarm()) ->
+    string().
+
+format_alarm(Ref, {down, E}, {down, _}) ->
+    Title = format_ref(Ref),
+    Since = format_since(" since ", E),
+    format("Seems that ~p still offline~s, you'd better check what's up.", [Title, Since]);
+
+format_alarm(Ref, {down, E}, _WasAlarm) ->
+    Title = format_ref(Ref),
+    Since = format_since(" at ", E),
+    format("Seems that ~p went offline~s, you'd better check what's up.", [Title, Since]);
+
+format_alarm(Ref, {up, _E}, _WasAlarm) ->
+    format("Seems that ~p went back online, cheers!", [format_ref(Ref)]).
+
+format_ref(Ref) ->
+    format_title(format("~p", [Ref])).
+
+format_since(Pre, Extra) ->
+    case deepprops:get(since, Extra) of
+        undefined -> "";
+        Datetime  -> Pre ++ format_datetime(Datetime)
+    end.
+
+format_title([L | Rest]) when L >= $a, L =< $z ->
+    [L + ($A - $a) | Rest];
+
+format_title(String) ->
+    String.
+
+format_datetime({{_, Mo, D}, {H, Mi, _}}) ->
+    format("~B.~2..0B ~2..0B:~2..0B UTC", [D, Mo, H, Mi]).
+
+format(Format, Args) ->
+    lists:flatten(io_lib:format(Format, Args)).
