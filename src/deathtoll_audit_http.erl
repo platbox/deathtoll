c9c5a7b6f77bb0a85c1eedd5219a0115c263e8c5
Mayorov Andrey <avmayorov@platbox.com>
2013-12-11 11:35:53 +0400

    Audit: allow to post data during http audit

--- src/deathtoll_audit_http.erl
+++ src/deathtoll_audit_http.erl
@@ -14,6 +14,8 @@
 
 -record(state, {
     url     :: string(),
+    body    :: iodata(),
+    ctype   :: string(),
     expect  :: ok | {status, 200..399},
     timeout :: pos_integer()
 }).
@@ -27,13 +29,15 @@ init(_Ref, Options) ->
     ok = deathtoll_app:start_app(inets),
     {ok, #state{
         url = deepprops:require(url, Options),
+        body = deepprops:get(body, Options, <<>>),
+        ctype = deepprops:get(content_type, Options, "text/plain"),
         expect = deepprops:get(expect, Options, {status, 200}),
         timeout = deepprops:get(timeout, Options, 10000)
     }}.
 
 -spec start(deathtoll:cref(), state()) -> {alarm, deathtoll:alarm()}.
 
-start(_Ref, #state{url = Url, expect = Expect, timeout = Timeout}) ->
+start(_Ref, #state{url = Url, body = Body, ctype = CType, expect = Expect, timeout = Timeout}) ->
     Headers = [
         {"Accept", "*/*"}
     ],
@@ -46,7 +50,13 @@ start(_Ref, #state{url = Url, expect = Expect, timeout = Timeout}) ->
         {body_format, binary},
         {full_result, false}
     ],
-    {alarm, case httpc:request(get, {Url, Headers}, HttpOptions, Options) of
+    {Method, Request} = if
+        Body =:= <<>>; Body =:= "" ->
+            {get, {Url, Headers}};
+        true ->
+            {post, {Url, Headers, CType, Body}}
+    end,
+    {alarm, case httpc:request(Method, Request, HttpOptions, Options) of
         {ok, _} when Expect =:= ok ->
             {up, []};
         {ok, {StatusCode, _}} when Expect =:= {status, StatusCode} ->
